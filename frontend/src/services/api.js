f94ebe9285e1607138d1549f943c926f3e0f3f09
software-dev01 <devsoftware603@gmail.com>
2026-03-31 11:15:58 +0530

    fix backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api"
-  baseURL: "https://newsbites-1aao.onrender.com"
+  baseURL: "https://newsbites-1aao.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
